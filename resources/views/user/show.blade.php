07fc2ea1dcab5eecd82decfdd7de8d07d3ea09c4
Harris Sontanu <42680163+harris-sontanu@users.noreply.github.com>
2022-10-21 06:52:01 +0800

    Fix user media social account list

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -6,40 +6,42 @@
     <h6 class="font-weight-bold mb-0">{{ $user->name }}</h6>
     <span class="d-block opacity-75">{{ Str::title($user->role) }}</span>
 
-    <ul class="list-inline list-inline-condensed mt-3 mb-0">
+    @if (!empty($user->facebook) OR !empty($user->twitter) OR !empty($user->instagram) OR !empty($user->youtube))
+        <ul class="list-inline list-inline-condensed mt-3 mb-0">
 
-        @if ( !empty($user->facebook))
-            <li class="list-inline-item">
-                <a href="{{ $user->facebook }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
-                    <i class="icon-facebook"></i>
-                </a>
-            </li>
-        @endif
+            @if ( !empty($user->facebook))
+                <li class="list-inline-item">
+                    <a href="{{ $user->facebook }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
+                        <i class="icon-facebook"></i>
+                    </a>
+                </li>
+            @endif
 
-        @if ( !empty($user->twitter))
-            <li class="list-inline-item">
-                <a href="{{ $user->twitter }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
-                    <i class="icon-twitter"></i>
-                </a>
-            </li>
-        @endif
+            @if ( !empty($user->twitter))
+                <li class="list-inline-item">
+                    <a href="{{ $user->twitter }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
+                        <i class="icon-twitter"></i>
+                    </a>
+                </li>
+            @endif
 
-        @if ( !empty($user->instagram))
-            <li class="list-inline-item">
-                <a href="{{ $user->instagram }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
-                    <i class="icon-instagram"></i>
-                </a>
-            </li>
-        @endif
+            @if ( !empty($user->instagram))
+                <li class="list-inline-item">
+                    <a href="{{ $user->instagram }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
+                        <i class="icon-instagram"></i>
+                    </a>
+                </li>
+            @endif
 
-        @if ( !empty($user->youtube))
-            <li class="list-inline-item">
-                <a href="{{ $user->youtube }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
-                    <i class="icon-youtube"></i>
-                </a>
-            </li>
-        @endif
-    </ul>
+            @if ( !empty($user->youtube))
+                <li class="list-inline-item">
+                    <a href="{{ $user->youtube }}" class="btn btn-outline-white btn-icon border-2 rounded-pill">
+                        <i class="icon-youtube"></i>
+                    </a>
+                </li>
+            @endif
+        </ul>
+    @endif
 
 </div>
 
