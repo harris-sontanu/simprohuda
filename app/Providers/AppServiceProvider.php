70088071ddfe5cef18db8669bbda235b0c1d6f1f
Harris Sontanu <42680163+harris-sontanu@users.noreply.github.com>
2022-10-24 09:45:11 +0800

    Fix failed call setting seeder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Pagination\Paginator;
 use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {   
-        $appName = Setting::where('key', 'appName')->first()->value;
+        $appName = config('app.name');
+        if (Schema::hasTable('settings')) {
+            $appName = Setting::where('key', 'appName')->first()->value;
+        }
         View::share('appName', $appName);
         
         Paginator::useBootstrapFour();
